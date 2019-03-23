bbf1e4bafa4288499a88f5b819907bafdfcec981
albin <albinpkr96@gmail.com>
2019-03-23 15:00:44 +0530

    clients pass args

--- cycle1/TCP/tcpclient.py
+++ cycle1/TCP/tcpclient.py
@@ -1,10 +1,11 @@
 import socket
+import sys
 
 s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 port = 8080
 
-s.connect(('127.0.0.1',port))
+s.connect((sys.argv[1],port))
 s.send('Hai from client')
 print s.recv(1024)
 
--- cycle1/Time/timeclient.py
+++ cycle1/Time/timeclient.py
@@ -1,4 +1,5 @@
 import socket
+import sys
 
 msgFromClient       = "Client Requesting Time...."
 
@@ -8,7 +9,7 @@ s= socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 port = 8080
 
-s.sendto(bytesToSend,('127.0.0.1',port))
+s.sendto(bytesToSend,(sys.argv[1],port))
 msgFromServer=s.recvfrom(1024)
 # msg = "Message from Server:  {}".format(msgFromServer[0])
 print"Time from Server: ",msgFromServer[0]
